bc6aeae0d1f9f9a0ba5fc777b84a8cb9a7160df4
david <davidmoerdijk@gmail.com>
2023-11-03 15:55:37 +0100

    ready for build

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import './styles/Page.css'
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-5xl mb-8 tracking-tighter">My portfolio</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">my portfolio</h1>
       <p className="prose prose-neutral dark:prose-invert">
+        This is your new portfolio.
       </p>
     </section>
   );
 }
-
-
-
